1636ddfc99c9bd09cf0220bc4ea3006564a49762
Martin <martin.masevski@gmail.com>
2016-12-08 16:50:49 +0100

    New animation for menu opener

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-$(".menu-opener").click(function() {
-	$(".menu-opener, .menu-opener-inner, .menu-head, .menu-body, .menu-head-link, .menu-body-link, .parent1, .parent2").toggleClass("active");
+$(document).ready(function() {
+	$('.nav-button').click(function() {
+		$("#nav-icon, .menu-head, .menu-body, .menu-head-link, .menu-body-link, .parent1, .parent2").toggleClass("open");
+	});
 });
