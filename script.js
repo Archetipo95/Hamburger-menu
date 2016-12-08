7755e75199e56ddfec927237e7e972cd98b3be04
Martin <martin.masevski@gmail.com>
2016-12-08 13:17:30 +0100

    Published

---
+++ script.js
@@ -0,0 +1,3 @@
+$(".menu-opener").click(function() {
+	$(".menu-opener, .menu-opener-inner, .menu-head, .menu-body, .menu-head-link, .menu-body-link, .parent1, .parent2").toggleClass("active");
+});
